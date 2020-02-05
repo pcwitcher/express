3cc4519c59aa18c6740e7e4f33ce1506df0b11fd
PCWitcher <pcwitcher.ie@gmail.com>
2020-02-05 19:50:59 +0100

    add 404 code

--- server.js
+++ server.js
@@ -3,23 +3,37 @@ const path = require('path');
 
 const app = express();
 
-app.use('/admin', (req, res, next) => {
-    if (isAdmin()) next();
-    else res.send('Go away!');
+app.use((req, res, next) => {
+    res.show = (name) => {
+        res.sendFile(path.join(__dirname + `/views/${name}`));
+    };
+    next();
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/views/home.html'));
+    res.show('index.html');
 });
 
-app.get('/cart', (req, res) => {
-    res.sendFile(path.join(__dirname + '/views/cart.html'));
+app.get('/about', (req, res) => {
+    res.show('about.html');
 });
 
-app.get('/admin/products', (req, res) => {
-    res.sendFile(path.join(__dirname + '/views/admin/products.html'));
+app.get('/contact', (req, res) => {
+    res.show('contact.html');
 });
 
-app.get('/admin/payments', (req, res) => {
-    res.sendFile(path.join(__dirname + '/views/admin/payments.html'));
+app.get('/info', (req, res) => {
+    res.show('info.html');
+});
+
+app.get('/history', (req, res) => {
+    res.show('history.html');
+});
+
+app.use((req, res) => {
+    res.status(404).send('404 not found...')
+});
+
+app.listen(8000, () => {
+    console.log('Server is running on port: 8000');
 });
